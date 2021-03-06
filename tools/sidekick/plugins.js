665100a5f2ab1d85b20251855637889b8e7f8203
florango <54337424+florango@users.noreply.github.com>
2021-03-06 09:32:25 -0800

    feat: update plugin

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -22,10 +22,28 @@
     sk.add({
       id: 'publish',
       button: {
-        text: 'Publish 2',
         action: async () => {
-          const { config } = sk;
-          sk.showModal('Publishing data...', true);
+          const { config, location } = sk;
+          const path = location.pathname;
+          sk.showModal(`Publishing ${path}`, true);
+          let urls = [path];
+          // purge dependencies
+          if (Array.isArray(window.hlx.dependencies)) {
+            urls = urls.concat(window.hlx.dependencies);
+          }
+
+          await Promise.all(urls.map((url) => sk.publish(url)));
+          if (config.host) {
+            sk.showModal('Please wait …', true);
+            // fetch and redirect to production
+            const prodURL = `https://${config.host}${path}`;
+            await fetch(prodURL, { cache: 'reload', mode: 'no-cors' });
+            // eslint-disable-next-line no-console
+            console.log(`redirecting to ${prodURL}`);
+            window.location.href = prodURL;
+          } else {
+            sk.notify('Successfully published');
+          }
         },
       },
     });
