cd3c86015b07df23337cca17f38c3869ccd88e4c
florango <54337424+florango@users.noreply.github.com>
2021-03-21 07:53:05 -0700

    chore: changed scripts to module

--- scripts.js
+++ scripts.js
@@ -27,16 +27,79 @@ function createTag(name, attrs) {
 function decorateFullWidthImage() {
   document.querySelectorAll('img').forEach(($img) => {
     const $section = $img.closest('div');
-    if($section.children.length === 1 || ($section.children.length === 2 && $section.children[1].tagName.startsWith('H'))) {
+    if ($section.children.length === 1 || ($section.children.length === 2 && $section.children[1].tagName.startsWith('H'))) {
       $section.parentNode.classList.add('full-image');
       $section.parentNode.classList.remove('section-wrapper');
     }
   })
 }
 
+/**
+ * Loads a CSS file.
+ * @param {string} href The path to the CSS file
+ */
+export function loadCSS(href) {
+  if (!document.querySelector(`head > link[href="${href}"]`)) {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', href);
+    link.onload = () => {
+    };
+    link.onerror = () => {
+    };
+    document.head.appendChild(link);
+  }
+}
+
+function decorateBlocks() {
+  document.querySelectorAll('main div.section-wrapper > div > div').forEach(async ($block) => {
+    const classes = Array.from($block.classList.values());
+    let blockName = classes[0];
+    const $section = $block.closest('.section-wrapper');
+    if ($section) {
+      $section.classList.add(`${blockName}-container`.replaceAll('--', '-'));
+    }
+    const blocksWithOptions = [];
+    blocksWithOptions.forEach((b) => {
+      if (blockName.startsWith(`${b}-`)) {
+        const options = blockName.substring(b.length + 1).split('-').filter((opt) => !!opt);
+        blockName = b;
+        $block.classList.add(b);
+        $block.classList.add(...options);
+      }
+    });
+    $block.classList.add('block');
+    import(`/blocks/${blockName}/${blockName}.js`)
+      .then((mod) => {
+        mod.default($block, blockName, document);
+      })
+      .catch((err) => console.log(`failed to load module for ${blockName}`, err));
+
+    loadCSS(`/blocks/${blockName}/${blockName}.css`);
+  });
+}
+
 function decoratePage() {
+  console.log(2)
   wrapSections('main > div');
   decorateFullWidthImage();
+  const $img = document.querySelector('main img');
+  if ($img) {
+    $img.addEventListener('load', () => {
+      loadLater();
+    })
+    $img.addEventListener('error', () => {
+      loadLater();
+    })
+  } else {
+    loadLater();
+    console.log(1)
+  }
+}
+
+function loadLater() {
+  decorateBlocks();
 }
 
 decoratePage();
+
