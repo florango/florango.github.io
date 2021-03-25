82c9a0fa8f290f8151efb83b3c03bf3b0d8b9a90
florango <54337424+florango@users.noreply.github.com>
2021-03-25 13:32:21 -0700

    feat: Added Cart Item markup

--- blocks/status/status.js
+++ blocks/status/status.js
@@ -49,25 +49,57 @@ export default async function decorate($block, blockName) {
         if (!orderStatus?.saved) {
           let $container = $main.querySelector('.cart-container');
           let inventory = await getInventory();
+          inventory = [
+            {
+              "id": "FLORANGISTA",
+              "commonName": "This is the Florangista",
+              "description": "Your weekly delivery!",
+              "color": "Varied",
+              "unit": "BOX",
+              "inQty": "30",
+              "outQty": "0",
+              "availableQty": "30",
+              "price": "$25.00"
+            },
+            ...inventory,
+          ]
           let $flowerPicsDoc = await getFlowerPics();
           let $cart = $container.querySelector('.cart');
           let $products = '';
           inventory.map((product, i) => {
             let $flowerPic = $flowerPicsDoc.querySelector('#' + product.id + ' picture');
-            let cartMarkup = `<div class="product">
-                            <div class="picture">
-                              ${$flowerPic.outerHTML}
-                            </div>
-                            <div class="description">
-                              ${product.description}
-                            </div>
-                            <div class="price">
-                              ${product.price}/${product.unit}
-                            </div>
-                          </div>`
-            $products += cartMarkup;
+            let cartItemMarkup = `
+              <div class="product">
+                <div class="picture">
+                  ${$flowerPic.outerHTML}
+                </div>
+                <div class="info">
+                  <h5>${product.commonName}</h5>
+                  <p class="description">
+                    ${product.description}
+                  </p>
+                </div>
+                <div class="price">
+                  ${product.price}/${product.unit}
+                </div>
+                <div>
+                  <select>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+              </div>`
+            $products += cartItemMarkup;
           })
-          console.log($products);
           $cart.innerHTML = $products;
           showContainer($container);
         } else {
